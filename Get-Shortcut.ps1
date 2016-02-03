600d4d8763edca5357f2a6ff2995777a4aa64863
Mr J Bennett <jbennett@birkdalehigh.co.uk>
2016-02-03 14:48:13 +0000

    adding some proper examples for get-help get-shortcut -examples

--- Get-Shortcut.ps1
+++ Get-Shortcut.ps1
@@ -5,7 +5,27 @@
    Copy the source shortcut into a variable to edit with the WScript shell methods
 
 .EXAMPLE
-   Gew-shortcut '.\GIMP.lnk'
+   Get-Shortcut .\Arduino.lnk
+
+   FullName         : C:\users\Public\Desktop\Arduino.lnk
+   Arguments        : 
+   Description      : 
+   Hotkey           : 
+   IconLocation     : ,0
+   RelativePath     : 
+   TargetPath       : C:\Program Files (x86)\Arduino\arduino.exe
+   WindowStyle      : 1
+   WorkingDirectory : C:\Program Files (x86)\Arduino
+
+.EXAMPLE
+   ls -Filter *.lnk | get-shortcut | select targetpath
+
+   TargetPath                                                                                       
+   ----------                                                                                       
+   C:\Program Files (x86)\Arduino\arduino.exe                                                       
+   C:\Program Files (x86)\Google\Chrome\Application\chrome.exe                                      
+   C:\Program Files\Inkscape\inkscape.exe                                                           
+   C:\Program Files\VideoLAN\VLC\vlc.exe                                                            
 
 #>
 function Get-Shortcut
