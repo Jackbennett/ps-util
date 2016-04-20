2b99a8463337b3b3828f21cdd14d0e6d9d3946b5
Jackbennett <jbennett@birkdalehigh.co.uk>
2016-04-20 10:48:12 +0100

    New image converting cmdlet using windows forms
    Copy-paste forum code like its 2001
    credit: http://hazzy.techanarchy.net/posh/powershell/bmp-to-jpg-the-powershell-way/

---
+++ ConvertTo-ImageFormat.ps1
@@ -0,0 +1,66 @@
+﻿function ConvertTo-ImageFormat {
+[CmdletBinding()]
+param
+(    [Parameter(Mandatory=$True,
+ValueFromPipeline=$false)]
+[string[]]$path,
+[string[]]$oldext,
+[validateset(“bitmap”,”emf”,”exif”,”gif”,”icon”,”jpeg”,”png”,”tiff”,”wmf”)]
+[string[]]$format
+)
+begin {
+if((Test-Path -Path $path))
+{
+}
+Else
+{
+Write-Warning “Path doesn’t exist – error”
+Return
+}
+$files=Get-ChildItem $path |where-object {$_.Extension -eq $oldext}
+$newext =””
+
+switch($format)
+{
+“bitmap”{$newext =”.bmp”;}
+“emf”{$newext =”.emf”;}
+“exif”{$newext =”.exif”}
+“gif”{$newext =”.gif”}
+“icon”{$newext =”.ico”;}
+“jpeg”{$newext =”.jpg”}
+“png”{$newext =”.png”}
+“tiff”{$newext =”.tif”}
+“wmf”{$newext =”.wmf”}
+}
+
+}
+ process {
+foreach ($file in $files) {
+Write-Verbose “Processing $file to convert to $format”
+$newimage = new-object System.Drawing.Bitmap($file.FullName);
+$newfile = Join-Path -Path $file.directory -ChildPath ($file.BaseName + $newext)
+
+      if(!(Test-Path -Path $newfile))
+{
+switch($format)
+{
+“bitmap”{$newimage.Save($newfile,”bmp”);}
+“emf”{$newimage.Save($newfile,”emf”);}
+“exif”{$newimage.Save($newfile,”exif”);}
+“gif”{$newimage.Save($newfile,”gif”);}
+“icon”{$newimage.Save($newfile,”icon”);}
+“jpeg”{$newimage.Save($newfile,”jpeg”);}
+“png”{$newimage.Save($newfile,”png”);}
+“tiff”{$newimage.Save($newfile,”tiff”);}
+“wmf”{$newimage.Save($newfile,”wmf”);}
+}
+}
+else
+{
+write-warning “$newfile already exists”
+}
+
+$newimage.Dispose()
+
+}
+}
