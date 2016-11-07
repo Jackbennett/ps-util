b71d7b0fc5a433c2917024796f70f98dc22b6538
Jackbennett <jbennett@birkdalehigh.co.uk>
2016-11-07 14:17:13 +0000

    Fix syntax of copied file

--- ConvertTo-ImageFormat.ps1
+++ ConvertTo-ImageFormat.ps1
@@ -3,10 +3,6 @@
    Convert between image formats
 .DESCRIPTION
    Convert between image formats with the Windows Forms library.
-.EXAMPLE
-   Example of how to use this cmdlet
-.EXAMPLE
-   Another example of how to use this cmdlet
 .INPUTS
    Image Files
 .OUTPUTS
@@ -29,61 +25,56 @@ function ConvertTo-ImageFormat {
         , [string[]]
         $oldext
 
-        , [validateset(“bitmap”,”emf”,”exif”,”gif”,”icon”,”jpeg”,”png”,”tiff”,”wmf”)]
+        , [validateSet("bitmap","emf","exif","gif","icon","jpeg","png","tiff","wmf")]
         [string[]]
         $format
     )
     begin {
-        if((Test-Path -Path $path)){
-
-        } Else {
-            Write-Warning “Path doesn’t exist – error”
-            Return
+        if( -not (Test-Path -Path $path)){
+            Throw "Path doesn’t exist – error"
         }
 
         $files = Get-ChildItem $path | where-object {$_.Extension -eq $oldext}
-        $newext = ””
+        $newext = ""
 
         switch($format){
-            “bitmap”{$newext = ”.bmp”;}
-            “emf”{$newext = ”.emf”;}
-            “exif”{$newext = ”.exif”}
-            “gif”{$newext =”.gif”}
-            “icon”{$newext =”.ico”;}
-            “jpeg”{$newext =”.jpg”}
-            “png”{$newext =”.png”}
-            “tiff”{$newext =”.tif”}
-            “wmf”{$newext =”.wmf”}
+            "bitmap" {$newext = ".bmp"  }
+            "emf"    {$newext = ".emf"  }
+            "exif"   {$newext = ".exif" }
+            "gif"    {$newext = ".gif"  }
+            "icon"   {$newext = ".ico"  }
+            "jpeg"   {$newext = ".jpg"  }
+            "png"    {$newext = ".png"  }
+            "tiff"   {$newext = ".tif"  }
+            "wmf"    {$newext = ".wmf"  }
         }
 
     }
- process {
-foreach ($file in $files) {
-Write-Verbose “Processing $file to convert to $format”
-$newimage = new-object System.Drawing.Bitmap($file.FullName);
-$newfile = Join-Path -Path $file.directory -ChildPath ($file.BaseName + $newext)
+    process {
+        foreach ($file in $files) {
+            Write-Verbose "Processing $file to convert to $format"
+            $newimage = new-object System.Drawing.Bitmap($file.FullName);
+            $newfile = Join-Path -Path $file.directory -ChildPath ($file.BaseName + $newext)
 
-      if(!(Test-Path -Path $newfile))
-{
-switch($format)
-{
-“bitmap”{$newimage.Save($newfile,”bmp”);}
-“emf”{$newimage.Save($newfile,”emf”);}
-“exif”{$newimage.Save($newfile,”exif”);}
-“gif”{$newimage.Save($newfile,”gif”);}
-“icon”{$newimage.Save($newfile,”icon”);}
-“jpeg”{$newimage.Save($newfile,”jpeg”);}
-“png”{$newimage.Save($newfile,”png”);}
-“tiff”{$newimage.Save($newfile,”tiff”);}
-“wmf”{$newimage.Save($newfile,”wmf”);}
-}
-}
-else
-{
-write-warning “$newfile already exists”
-}
+            if(-not (Test-Path -Path $newfile)){
+                switch($format){
+                    "bitmap" {$newimage.Save($newfile,"bmp" );}
+                    "emf"    {$newimage.Save($newfile,"emf" );}
+                    "exif"   {$newimage.Save($newfile,"exif");}
+                    "gif"    {$newimage.Save($newfile,"gif" );}
+                    "icon"   {$newimage.Save($newfile,"icon");}
+                    "jpeg"   {$newimage.Save($newfile,"jpeg");}
+                    "png"    {$newimage.Save($newfile,"png" );}
+                    "tiff"   {$newimage.Save($newfile,"tiff");}
+                    "wmf"    {$newimage.Save($newfile,"wmf" );}
+                }
+            }
+            else{
+                write-warning "$newfile already exists"
+            }
 
-$newimage.Dispose()
+            $newimage.Dispose()
 
-}
+        }
+    }
 }
