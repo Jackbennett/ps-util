342abcbe4b887d072d284ac71eab39b0bd27a4f1
Jackbennett <jbennett@birkdalehigh.co.uk>
2018-06-20 13:57:16 +0100

    New cmdlet: New-EasyPassword, semver 0.1 bump
    Generate passwords to use that humans find easy to read for the purposes
    of resetting accounts
    
    * Includes Unit Tests

---
+++ New-EasyPassword.Tests.ps1
@@ -0,0 +1,23 @@
+﻿. (Join-Path $PSScriptRoot 'New-EasyPassword.ps1')
+
+Describe "New-EasyPassword" {
+    It "Error when making a password shorter than it is long" {
+        { New-EasyPassword -MinLength 10 -MaxLength 5 } | Should -Throw
+    }
+    It "No Error when the min-max range is correct" {
+        { New-EasyPassword -MinLength 5 -MaxLength 10 } | Should -Not -Throw
+    }
+    It "No Error specifying only the minimum length" {
+        { New-EasyPassword -MinLength 5 } | Should -Not -Throw
+    }
+    It "No Error specifying only the maximum length" {
+        { New-EasyPassword -MaxLength 8 } | Should -Not -Throw
+    }
+    It "No Error with maximum length lower than default minumum value" {
+        { New-EasyPassword -MaxLength 4 } | Should -Not -Throw
+    }
+    It "Default parameters work without issue" {
+        { New-EasyPassword } | Should -Not -Throw
+    }
+
+}
--- New-EasyPassword.ps1
+++ New-EasyPassword.ps1
@@ -1,7 +1,28 @@
-function New-EasyPassword($MinLength = 5, $MaxLength = 8){
+function New-EasyPassword{
+    [CmdletBinding()]
+    Param(
+        # Minimum Password Lenght
+        [Parameter(Position=0)]
+        [ValidateRange(1, [int]::MaxValue)]
+        [Alias('Min')]
+        $MinLength = 6
+
+        , # Maximum Password Lenght
+        [Parameter(Position=1)]
+        [ValidateRange(2, [int]::MaxValue)]
+        [Alias('Max')]
+        $MaxLength = $MinLength + 2
+    )
+    if($PSBoundParameters.ContainsKey('MaxLength') -and -not $PSBoundParameters.ContainsKey('MinLength')){
+        $MinLength = $MaxLength - 1
+    }
+    if($MinLength -ge $MaxLength){
+        Throw [System.Management.Automation.ParameterBindingException]::New("Max length($MaxLength) must be greater than the Minimum($MinLength)")
+    }
+
     $length = Get-Random -Minimum $MinLength -Maximum $MaxLength
 
-    $letters = (33..122) | 
+    $letters = (33..122) |
         Where-Object {
             # Using an ASCII table, exclude character numbers found hard to say or type.
             # Consider removing O, 0, I, l, W, w, V v if you can't control the font the user is presented with.
